0b941d5314f29b111c43c768ee607c587fbd150d
Doubleshot1122 <skibo1122@yahoo.com>
2017-05-20 11:59:40 -0700

    updated db info

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/hacknet_db',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  }
+}
