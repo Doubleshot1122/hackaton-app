66508f31f4d3fd93b75745fb9b1ff1f81b4a622e
Ronald K. Ning <the@loude.st>
2017-05-20 16:04:33 -0700

    keyword stuff and crawler

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,9 +1,49 @@
 var Feed = require('rss-to-json');
+var MetaInspector = require('node-metainspector');
+
+var rssItems = [];
+
+function parseRSSitem(value) {
+    var url = value.url;
+    var client = new MetaInspector(url, { timeout: 5000 });
+    var r = {};
+    r["title"] = value.title;
+    r["description"] = value.description;
+    r["keywords"] = [];
+    r["image"] = null;
+
+    client.on("fetch", function(){
+        keys = client.keywords;
+
+        // parse each keyword as a word if it turns out to be a sentence
+        for (var i = 0; i < keys.length; i++) {
+            text = keys[i];
+            token = text.split(" ");
+            for (var j = 0; j < token.length; j++) {
+                word = token[j];
+                if (r["keywords"].indexOf(word) < 0 && word.length > 3) {
+                    r["keywords"].push(word);
+                }
+            }
+        }
+
+        r["image"] = client.image;
+        console.log(r);
+        rssItems.push(r);
+    });
+
+    client.fetch();
+}
 
 function parseRSSfeed(url) {
     Feed.load(url, function(err, rss){
-        console.log(rss);
+        var items = rss.items;
+        for(var i=0; i < items.length; i++) {
+            item = items[i];
+            parseRSSitem(item);
+        }
     });
+    console.log(rssItems);
 }
 
 module.exports = {
