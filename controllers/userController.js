55b6cd398d6e661d06e49c9a4393b032f07161e3
Mike <mahathaway93@gmail.com>
2017-05-20 13:10:29 -0700

    add show and customize usser routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,36 @@
 function showUserProfile(req, res, next){
   const id = req.params.id
+
+  return db('users')
+    .where('users.id', id)
+    .then((userData) => {
+      res.render('/user/index', userData)
+    })
+    .catch((err) => next(err))
 }
 
 function customizeUser(req, res, next){
   const id = req.params.id
+  const userTags = req.body
+
+  return db('users')
+    .update(userTags)
+    .where('users.id', id)
+    .then(() => {
+      res.redirect(`/users/${id}`)
+    })
+    .catch((err) => next(err))
 }
 
 function newUser(req,res,next){
+  const newUser = req.body
 
+  return db('users')
+    .insert(newUser, '*')
+    .then((newUser) => {
+      res.redirect(`/users/${newUser.id}`)
+    })
+    .catch((err) => next(err))
 }
 
 //show all articles
