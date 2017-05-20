e46ca309fa53a205b0c3bd8f3d40e7b926331f71
Doubleshot1122 <skibo1122@yahoo.com>
2017-05-20 12:14:02 -0700

    fixed head

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 const express = require('express')
 const router = express.Router()
 
