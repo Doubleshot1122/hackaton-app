99f078efd0f38354f073bafd2a339e82e0b4e960
Mike <mahathaway93@gmail.com>
2017-05-20 11:58:59 -0700

    added initial route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const userController = require('../controllers/userController')
 
-module.exports = router;
+
+//user profile
+router.get('/user/:id', userController.showUserProfile)
+router.put('/user/:id', userController.customizeUser)
+
+//add user
+router.post('/user', userController.newUser)
+
+//user index page
+router.get('/user/:id/article', userController.showAllArticles)
+
+//specific article ppage
+router.get('/user/:id/article/:articleId', userController.showSpecificArticle)
+
+
+
+
+
+module.exports = router
