f6b98eed4be0ab4f94fe658518d1e639f55d9ec2
Doubleshot1122 <skibo1122@yahoo.com>
2017-05-20 16:16:21 -0700

    creating show single article

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/search', search);
+app.use('/articles', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/articleController.js
+++ controllers/articleController.js
@@ -3,10 +3,11 @@ const db = require('../db');
 //index dropdown
 function getSingleArticle(req, res, next){
   const id = req.params.id
+  console.log('id', id);
   return db('articles')
   .where( {id} )
   .then(article => {
-    console.log(article);
+    // console.log(article);
     res.render('/', article)
   })
   .catch((err) => next(err))
