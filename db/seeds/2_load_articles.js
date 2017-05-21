b36d7340e734ab29faac4763a23eddc18adfaa5d
Sean McDermott <mcderm.sean@gmail.com>
2017-05-21 13:31:35 -0700

    updated seeds to include more description, reformatted briefing page

--- db/seeds/2_load_articles.js
+++ db/seeds/2_load_articles.js
@@ -7,14 +7,14 @@ exports.seed = (knex) => {
           png: 'http://i2.cdn.cnn.com/cnnnext/dam/assets/170518051436-donald-trump-silhouette-0516-medium-plus-169.jpg',
           link: "http://www.cnn.com/2017/05/20/opinions/there-are-lots-of-ways-to-get-to-impeachment-cevallos/index.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+rss%2Fcnn_topstories+%28RSS%3A+CNN+-+Top+Stories%29",
           keywords: JSON.stringify(['gang', 'construction', 'human traffic']),
-          description: "CNN - President Donald Trump"
+          description: "CNN - President Donald Trump Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
           title: "Comey's father: Trump was 'scared to death' of FBI director",
           png: 'http://i2.cdn.cnn.com/cnnnext/dam/assets/170518051436-donald-trump-silhouette-0516-medium-plus-169.jpg',
           link: "http://www.cnn.com/2017/05/20/politics/comey-father-trump/index.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+rss%2Fcnn_allpolitics+%28RSS%3A+CNN+-+Politics%29",
           keywords: JSON.stringify(['impeachment', 'CNN', 'human traffic']),
-          description: "CNN - As White House"
+          description: "CNN - As White House Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
       ])
     })
